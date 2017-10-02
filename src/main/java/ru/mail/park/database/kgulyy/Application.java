86fa370f7c55d2a167732ae3b1dd54e43b1ae1d9
Konstantin Gulyy <kgulyy@my.com>
2017-10-02 16:21:32 +0300

    Spring Boot added

---
+++ src/main/java/ru/mail/park/database/kgulyy/Application.java
@@ -0,0 +1,14 @@
+package ru.mail.park.database.kgulyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by KGulyy on 02.10.2017.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
