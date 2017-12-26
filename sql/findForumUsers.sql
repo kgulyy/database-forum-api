bb4cf371111895b20afa7402f995094e837cb5f3
Konstantin Gulyy <kgulyy@my.com>
2017-12-26 17:29:52 +0300

    No name forum id

--- sql/findForumUsers.sql
+++ sql/findForumUsers.sql
@@ -4,14 +4,14 @@ SELECT * FROM
 		SELECT DISTINCT u.nickname, u.fullname, u.email, u.about
 		FROM users u, threads t
 		WHERE u.nickname = t.author
-		AND LOWER(t.forum) = LOWER('I_2X23oW414r8')
+		AND LOWER(t.forum) = LOWER(:forum)
 	)
 	UNION
 	(
 		SELECT DISTINCT u.nickname, u.fullname, u.email, u.about
 		FROM users u, posts p
 		WHERE u.nickname = p.author
-		AND LOWER(p.forum) = LOWER('I_2X23oW414r8')) 
+		AND LOWER(p.forum) = LOWER(:forum))
 	) AS u
 -- WHERE LOWER(nickname) > LOWER(:since) COLLATE UCS_BASIC
 ORDER BY LOWER(u.nickname) COLLATE UCS_BASIC
