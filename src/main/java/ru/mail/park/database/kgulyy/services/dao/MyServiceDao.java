a72a05f436b424918fb65441c4c366d9495fa173
Konstantin Gulyy <kgulyy@my.com>
2018-01-11 12:13:03 +0300

    Get status is improved

--- src/main/java/ru/mail/park/database/kgulyy/services/dao/MyServiceDao.java
+++ src/main/java/ru/mail/park/database/kgulyy/services/dao/MyServiceDao.java
@@ -1,6 +1,7 @@
 package ru.mail.park.database.kgulyy.services.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import ru.mail.park.database.kgulyy.domains.Status;
 import ru.mail.park.database.kgulyy.services.MyService;
@@ -16,14 +17,24 @@ public class MyServiceDao implements MyService {
         this.template = template;
     }
 
+    private static final RowMapper<Status> STATUS_ROW_MAPPER = (res, num) -> {
+        int users = res.getInt("users");
+        int forums = res.getInt("forums");
+        int threads = res.getInt("threads");
+        long posts = res.getLong("posts");
+
+        return new Status(users, forums, threads, posts);
+    };
+
     @Override
     public Status getStatus() {
-        final Integer numberOfUsers = template.queryForObject("SELECT COUNT(*) FROM users", Integer.class);
-        final Integer numberOfForums = template.queryForObject("SELECT COUNT(*) FROM forums", Integer.class);
-        final Integer numberOfThreads = template.queryForObject("SELECT COUNT(*) FROM threads", Integer.class);
-        final Long numberOfPosts = template.queryForObject("SELECT COUNT(*) FROM posts", Long.class);
+        final String sql = "SELECT COUNT(*) as users, " +
+                "(SELECT COUNT(*) FROM forums) as forums, " +
+                "(SELECT COUNT(*) FROM posts) as posts, " +
+                "(SELECT COUNT(*) FROM threads) as threads " +
+                "FROM users";
 
-        return new Status(numberOfUsers, numberOfForums, numberOfThreads, numberOfPosts);
+        return template.queryForObject(sql, STATUS_ROW_MAPPER);
     }
 
     @Override
